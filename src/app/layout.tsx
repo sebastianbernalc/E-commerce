bd23f406a24d9509fc46a4f49ed97461ba059476
sebastianbernalc <sebastianbernalc@udea.edu.co>
2024-08-19 01:56:26 -0500

    Add Analytics stats

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Providers from "@/components/Providers";
 import "./globals.scss";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
             <body className={inter.className}>
                 <Providers>
                     {children}
+                    <Analytics />
                 </Providers>
             </body>
         </html>
